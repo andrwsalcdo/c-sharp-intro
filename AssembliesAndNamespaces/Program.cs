f21d952a2bbd1fca6e3e13b16d15c043e32565fe
Andrew Salcedo <asalcedo07@gmail.com>
2017-09-06 15:20:03 -0400

    assemblies & namespaces. ex: using System.Net

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net; 
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WebClient client = new WebClient(); 
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            
+            Console.ReadLine();        
+        }
+    }
+}
